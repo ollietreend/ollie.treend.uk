edeab3962c108380cf1873b78f1084ac9f9dabba
Ollie Treend <ollie@treend.uk>
2024-01-27 16:30:53 +0000

    Use Cabin font

--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -1,10 +1,15 @@
 import typography from "@tailwindcss/typography";
+import defaultTheme from "tailwindcss/defaultTheme";
 
 /** @type {import('tailwindcss').Config} */
 export default {
 	content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
 	theme: {
-		extend: {},
+		extend: {
+			fontFamily: {
+				sans: ["Cabin Variable", ...defaultTheme.fontFamily.sans],
+      },
+		},
 	},
 	plugins: [typography],
 }
