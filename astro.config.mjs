3d5183b61d1e26e17dbcd75dd1c75cc8eb445999
Ollie Treend <ollie@treend.uk>
2024-02-01 13:17:20 +0000

    Add robots.txt and sitemap.xml
    And only allow indexing in production.

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,15 +2,28 @@ import { defineConfig } from "astro/config";
 import tailwind from "@astrojs/tailwind";
 import styleMarkdownElements from "./src/lib/styleMarkdownElements";
 import mdx from "@astrojs/mdx";
+import sitemap from "@astrojs/sitemap";
+import robotsTxt from "astro-robots-txt";
+
+// Only allow robots in production
+const policy = [];
+if (process.env.NETLIFY && process.env.CONTEXT == "production") {
+  policy.push({ allow: "/", userAgent: "*" });
+} else {
+  policy.push({ disallow: "/", userAgent: "*" });
+}
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [mdx(), tailwind()],
+  site: process.env.URL ?? "http://localhost:4321",
+  integrations: [mdx(), tailwind(), sitemap(), robotsTxt({ policy })],
   build: {
     assets: "assets",
   },
   markdown: {
     rehypePlugins: [styleMarkdownElements],
-    shikiConfig: { theme: "monokai" },
+    shikiConfig: {
+      theme: "monokai",
+    },
   },
 });
