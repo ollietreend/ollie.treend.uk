ae763ada4defb494354d27412c1e1dbae383cce2
Ollie Treend <ollie@treend.uk>
2025-01-04 23:59:45 +0000

    Remove Astro-Shield and revert to a hardcoded CSP

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,7 +5,6 @@ import mdx from "@astrojs/mdx";
 import sitemap from "@astrojs/sitemap";
 import robotsTxt from "astro-robots-txt";
 import svelte from "@astrojs/svelte";
-import { shield } from "@kindspells/astro-shield";
 
 // Only allow robots in production
 const policy = [];
@@ -15,24 +14,6 @@ if (process.env.NETLIFY && process.env.CONTEXT == "production") {
   policy.push({ disallow: "/", userAgent: "*" });
 }
 
-// Content-Security-Policy header config
-// Astro-Shield auto-calculates SRI hashes and generates the required CSP headers
-const shieldConfig = {
-  securityHeaders: {
-    enableOnStaticPages: { provider: "netlify" },
-    contentSecurityPolicy: {
-      cspDirectives: {
-        "default-src": "'self'",
-
-        // Required so the waving hand Easter egg is performant
-        // https://github.com/catdad/canvas-confetti/issues/131#issuecomment-727661873
-        // Skipping 'self' since I don't use any other workers in this project
-        "worker-src": "blob:",
-      },
-    },
-  },
-};
-
 // https://astro.build/config
 export default defineConfig({
   site: process.env.URL ?? "http://localhost:4321",
@@ -40,7 +21,6 @@ export default defineConfig({
     mdx(),
     svelte(),
     tailwind(),
-    shield(shieldConfig),
     sitemap(),
     robotsTxt({ policy }),
   ],
